110791ab5a85d9f38b87c0d9ef7c52a36f8119f7
JULIETA ROMANO <49703405@ort>
2026-06-26 08:29:18 -0300

    creacion de clase figuritas

--- Models/figuritas.cs
+++ Models/figuritas.cs
@@ -1,6 +1,11 @@
 namespace Album.Models;
 
-public class figuritas 
+public class figuritas
 {
+    public string Nombre { get; set; }
+    public int Numero { get; set; }
+    public string Imagen { get; set; }
+
     
+
 }
