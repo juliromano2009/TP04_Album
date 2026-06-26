1f64ebce80310480ac3630098ef0095b5d98c663
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-06-26 09:02:49 -0300

    view creada

--- Models/figuritas.cs
+++ Models/figuritas.cs
@@ -1,17 +1,20 @@
 namespace Album.Models;
 
-public class figuritas
+public class Figuritas
 {
     public string Nombre { get; set; }
     public int Numero { get; set; }
     public string Imagen { get; set; }
-
+    public bool yaPegada {get; set;};
+    public int cantEnPosesion {get; set;};
     
-    public figuritas(string nombre, int numero, string imagen)
+    Figuritas(string nombre, int numero, string imagen)
     {
         this.Nombre = nombre;
         this.Numero = numero;
         this.Imagen = "fondo.png";
+        yaPegada = false;
+        cantEnPosesion = 0;
     }
 
 }
