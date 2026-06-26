7be1d9c85740f844fb5be41b7fb31731fe5ab7d4
JULIETA ROMANO <49703405@ort>
2026-06-26 08:54:19 -0300

    terminando bd

--- Models/BD.cs
+++ Models/BD.cs
@@ -7,14 +7,44 @@ public class BD
 {
     public string _connectionString = @"Server=localhost; DataBase = Album; Integrated Security=True; TrustServerCertificate=True;"; 
 
-    public List<string> hacerLista()
+    public List<figuritas> ListaFiguritas()
     {
-        List <string> palabras = new List<string>();
+        List <figuritas> figuritas = new List<figuritas>();
         using(SqlConnection connection = new SqlConnection(_connectionString))
         {
-            string query = "SELECT Texto FROM Palabras";
-            palabras = connection.Query<string>(query).ToList();
+            string query = "SELECT Nombre, Numero, Imagen FROM Figuritas";
+            figuritas = connection.Query<figuritas>(query).ToList();
         }
-        return palabras; 
+        return figuritas; 
+    }
+
+    public List<figuritas> ListaFiguritasPorUsuario(int numero)
+    {
+        List <figuritas> figuritas = new List<figuritas>();
+        using(SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT Nombre, Numero, Imagen FROM figuritas_x_usuario ";
+            figuritas = connection.Query<figuritas>(query, new { Numero = numero }).ToList();
+        }
+        return figuritas; 
+    }
+
+    public List<figuritas> AbrirSobre ()
+    {
+        Random random = new Random ();
+        int x = random.Next(1, 48);
+        List <figuritas> sobre = new List<figuritas>();
+        List<figuritas> figuritas = new List<figuritas>();
+        using(SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT Nombre, Numero, Imagen FROM Figuritas";
+            figuritas = connection.Query<figuritas>(query).ToList();
+        }
+        for (int i = 0 ; i < 5 ; i++)
+        {
+            sobre.Add(figuritas[x]);
+            
+        }
+        return sobre;
     }
 }
