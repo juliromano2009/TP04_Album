c3b6136e79a26e85d260fcb46dbcf09fbcecf9c6
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-07-02 08:27:34 -0300

    arreglo clase

--- Models/BD.cs
+++ Models/BD.cs
@@ -9,7 +9,7 @@ public class BD
 
     public List<Figuritas> ListaFiguritas()
     {
-        List <Figuritas> figuritas = new List<figuritas>();
+        List <Figuritas> figuritas = new List<Figuritas>();
         using(SqlConnection connection = new SqlConnection(_connectionString))
         {
             string query = "SELECT Nombre, Numero, Imagen FROM Figuritas";
